1ba9cbc532673e080bfb28beba3596d680289c6d
Marlon <57847810+MarlonMagDich@users.noreply.github.com>
2023-09-13 09:48:51 +0000

    penis

---
+++ A00_Klasse/Schueler.java
@@ -0,0 +1,23 @@
+class Schueler{
+
+    int groesse;
+    String name;
+
+    //Konstruktor
+    public Schueler(){
+
+        groesse = 175;
+        name = "Mustermann";
+
+    }    
+
+    //Getter
+    public String getName(){
+
+        return name;
+        
+    }
+
+
+
+}
