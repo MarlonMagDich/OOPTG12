77e0eea9c6ac03522dfd492259d52bcb2a2aa692
Marlon <57847810+MarlonMagDich@users.noreply.github.com>
2023-09-13 10:05:19 +0000

    erzeuge

---
+++ A00_Klasse/ErzeugeSchueler.java
@@ -0,0 +1,15 @@
+
+/**
+ * ErzeugeSchueler
+ */
+public class ErzeugeSchueler {
+
+    public static void main(String[] args) {
+        //Schueler Objekt erzeugen
+        Schueler s1 = new Schueler();
+        //Zugriff auf das Objekt
+        System.out.println(s1.getName());
+
+    }
+
+}
